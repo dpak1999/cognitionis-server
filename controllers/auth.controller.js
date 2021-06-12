661a3fbddcf27f81a38e2cd83235d53d94a93e36
Deepak kumar dash <deepakkumardash313@gmail.com>
2021-06-12 23:45:55 +0530

    add : login route & controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,7 @@
 import colors from "colors";
 import User from "../models/User";
 import { comparePassword, hashPassword } from "../utils/auth.util";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   try {
@@ -43,6 +44,38 @@ export const register = async (req, res) => {
     await user.save();
 
     return res.json({ ok: true });
+  } catch (error) {
+    console.log(`${error.message}`.red.underline);
+    return res.status(400).send("Try again");
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // get the user with email
+    const user = await User.findOne({ email }).exec();
+    if (!user) {
+      return res.status(400).send("User with that email doesnt exist");
+    }
+
+    // check pwd
+    const matchPwd = await comparePassword(password, user.password);
+
+    if (!matchPwd) {
+      return res.status(400).send("Incorrect password");
+    }
+
+    // create signed jwt
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "10d",
+    });
+
+    // return to client
+    user.password = undefined;
+    res.cookie("token", token, { httpOnly: true });
+    res.json(user);
   } catch (error) {
     console.log(`${error.message}`.red.underline);
     return res.status(400).send("Try again");
