e0d82e67fcce756b93e2b8af6256d0ca1a353ff1
dpak1999 <deepakkumardash313@gmail.com>
2021-07-29 00:27:46 +0530

    upload image to AWS S3

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -1,3 +1,51 @@
 /** @format */
+import AWS from "aws-sdk";
+import { nanoid } from "nanoid";
 
-export const uploadImage = () => {};
+const awsConfig = {
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+  apiVersion: process.env.AWS_API_VERSION,
+};
+
+const S3 = new AWS.S3(awsConfig);
+
+export const uploadImage = async (req, res) => {
+  try {
+    const { image } = req.body;
+
+    if (!image) return res.status(400).send("No image found");
+
+    // remove noisy text
+    const base64Data = new Buffer.from(
+      image.replace(/^data:image\/\w+;base64,/, ""),
+      "base64"
+    );
+
+    const type = image.split(";")[0].split("/")[1];
+
+    // image params
+    const params = {
+      Bucket: "cognitionis-bucket",
+      Key: `${nanoid()}.${type}`,
+      Body: base64Data,
+      ACL: "public-read",
+      ContentEncoding: "base64",
+      ContentType: `image/${type}`,
+    };
+
+    // upload to AWS S3
+    S3.upload(params, (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.sendStatus(400);
+      }
+
+      console.log(data);
+      res.send(data);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
