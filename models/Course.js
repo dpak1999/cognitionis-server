d5834b5a75cbfcb9847c366748e673c51e0c3f96
dpak1999 <deepakkumardash313@gmail.com>
2021-09-02 23:24:01 +0530

    add : course schema

---
+++ models/Course.js
@@ -0,0 +1,76 @@
+/** @format */
+
+import mongoose from "mongoose";
+const { ObjectId } = mongoose.Schema;
+
+const lessonSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+      minLength: 3,
+      maxLength: 300,
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+      required: true,
+    },
+    content: {
+      type: {},
+      minLength: 200,
+    },
+    video_link: {},
+    free_preview: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const courseScehma = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      minLength: 3,
+      maxLength: 300,
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+      required: true,
+    },
+    description: {
+      type: {},
+      minLength: 200,
+      required: true,
+    },
+    price: {
+      type: Number,
+      default: 9.99,
+    },
+    image: {},
+    category: String,
+    published: {
+      type: Boolean,
+      default: false,
+    },
+    paid: {
+      type: Boolean,
+      default: true,
+    },
+    instructor: {
+      type: ObjectId,
+      ref: "User",
+      required: true,
+    },
+    lessons: [lessonSchema],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Course", courseScehma);
