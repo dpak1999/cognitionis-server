1866df99a6811724b4f3a6eff6c818574c6f4b07
dpak1999 <deepakkumardash313@gmail.com>
2021-09-02 23:36:13 +0530

    add : instructor midleware & route

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,24 @@
 /** @format */
 
 import expressJwt from "express-jwt";
+import User from "../models/User";
 
 export const requireSignin = expressJwt({
   getToken: (req, res) => req.cookies.token,
   secret: process.env.JWT_SECRET,
   algorithms: ["HS256"],
 });
+
+export const isInstructor = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id).exec();
+
+    if (!user.role.includes("Instructor")) {
+      return res.sendStatus(403);
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
--- routes/course.route.js
+++ routes/course.route.js
@@ -1,12 +1,20 @@
 /** @format */
 
 import express from "express";
-import { uploadImage, removeImage } from "../controllers/course.controller";
-import { requireSignin } from "../middlewares";
+import {
+  uploadImage,
+  removeImage,
+  create,
+} from "../controllers/course.controller";
+import { isInstructor, requireSignin } from "../middlewares";
 
 const router = express.Router();
 
+// image
 router.post("/course/upload-image", uploadImage);
 router.post("/course/remove-image", removeImage);
+
+// course
+router.post("/course", requireSignin, isInstructor, create);
 
 module.exports = router;
