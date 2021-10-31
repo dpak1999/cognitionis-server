d0adfd4d304925b978ee2e6c4a77497f1f2e87a1
dpak1999 <deepakkumardash313@gmail.com>
2021-10-31 21:35:23 +0530

    add : student count &instructor bal

--- routes/instructor.route.js
+++ routes/instructor.route.js
@@ -1,19 +1,23 @@
 /** @format */
 
-import express from "express";
+import express from 'express';
 import {
   makeInstructor,
   getAccountStatus,
   currentInstructor,
   instructorCourse,
-} from "../controllers/instructor.controller";
-import { requireSignin } from "../middlewares";
+  studentCount,
+  instructorBalance,
+} from '../controllers/instructor.controller';
+import { requireSignin } from '../middlewares';
 
 const router = express.Router();
 
-router.post("/make-instructor", requireSignin, makeInstructor);
-router.post("/get-account-status", requireSignin, getAccountStatus);
-router.get("/current-instructor", requireSignin, currentInstructor);
-router.get("/instructor-courses", requireSignin, instructorCourse);
+router.post('/make-instructor', requireSignin, makeInstructor);
+router.post('/get-account-status', requireSignin, getAccountStatus);
+router.get('/current-instructor', requireSignin, currentInstructor);
+router.get('/instructor-courses', requireSignin, instructorCourse);
+router.post('/instructor/student-count', requireSignin, studentCount);
+router.get('/instructor/balance', requireSignin, instructorBalance);
 
 module.exports = router;
